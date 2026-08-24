a69ae7ce2b232a636ccb9cc3c50b9f9bc257b598
LucasMalves148 <lucasanderlyne90@gmail.com>
2026-08-23 21:07:11 -0300

    feat: implementa ClienteController e endpoint de atualização de clientes

--- src/main/java/lojamercado/mercado/controller/ClienteController.java
+++ src/main/java/lojamercado/mercado/controller/ClienteController.java
@@ -1,6 +1,67 @@
 package lojamercado.mercado.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lojamercado.mercado.dto.request.ClienteRequest;
+import lojamercado.mercado.dto.response.ClienteResponse;
+import lojamercado.mercado.service.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-    
-    
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<ClienteResponse> criarCliente(@Valid @RequestBody ClienteRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.criarCliente(request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+        clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ClienteResponse>> listarClientes() {
+        return ResponseEntity.ok(clienteService.listarClientes());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponse> encontrarClientePorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.encontrarClientePorId(id));
+    }
+
+    @GetMapping("/email")
+    public ResponseEntity<ClienteResponse> encontrarClientePorEmail(@RequestParam String email) {
+        return ResponseEntity.ok(clienteService.encontrarClientePorEmail(email));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteResponse> atualizarCliente(
+            @PathVariable Long id,
+            @Valid @RequestBody ClienteRequest request) {
+
+        return ResponseEntity.ok(
+                clienteService.atualizarCliente(id, request));
+    }
+
 }
--- src/main/java/lojamercado/mercado/service/ClienteService.java
+++ src/main/java/lojamercado/mercado/service/ClienteService.java
@@ -1,6 +1,5 @@
 package lojamercado.mercado.service;
 
-
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,19 +13,18 @@ import lojamercado.mercado.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
-    
-    
+
     private final ClienteRepository clienteRepository;
     private final Mapper mapper;
 
-    public ClienteService(ClienteRepository clienteRepository, Mapper mapper){
+    public ClienteService(ClienteRepository clienteRepository, Mapper mapper) {
         this.mapper = mapper;
         this.clienteRepository = clienteRepository;
     }
 
-    public ClienteResponse criarCliente(ClienteRequest cRequest){
+    public ClienteResponse criarCliente(ClienteRequest cRequest) {
         Cliente cliente = new Cliente();
-        
+
         cliente.setEmail(cRequest.getEmail());
         cliente.setNome(cRequest.getNome());
 
@@ -35,33 +33,42 @@ public class ClienteService {
         return mapper.clienteToResponse(cliente);
     }
 
-    public void deletarCliente(Long id){
-        if(!clienteRepository.existsById(id)){
+    public void deletarCliente(Long id) {
+        if (!clienteRepository.existsById(id)) {
             throw new ClienteNotFoundException("Não foi encontrado nenhum cliente com esse id");
         }
 
         clienteRepository.deleteById(id);
     }
-    
-    public List<ClienteResponse> listarClientes(){
-       return clienteRepository.findAll()
-            .stream()
-            .map(mapper::clienteToResponse)
-            .toList();
-    }
-    
-    public ClienteResponse encontrarClientePorId(Long id){
-        return clienteRepository.findById(id)
-            .map(mapper::clienteToResponse)
-            .orElseThrow(() -> new ClienteNotFoundException("Não foi encontrado nenhum cliente com este Id"));
-        }
 
-    public ClienteResponse encontrarClientePorEmail(String email){
-            return clienteRepository.findByEmailIgnoreCase(email)
+    public List<ClienteResponse> listarClientes() {
+        return clienteRepository.findAll()
+                .stream()
+                .map(mapper::clienteToResponse)
+                .toList();
+    }
+
+    public ClienteResponse encontrarClientePorId(Long id) {
+        return clienteRepository.findById(id)
+                .map(mapper::clienteToResponse)
+                .orElseThrow(() -> new ClienteNotFoundException("Não foi encontrado nenhum cliente com este Id"));
+    }
+
+    public ClienteResponse encontrarClientePorEmail(String email) {
+        return clienteRepository.findByEmailIgnoreCase(email)
                 .map(mapper::clienteToResponse)
                 .orElseThrow(() -> new ClienteNotFoundException("Não foi encontrado nenhum cliente com este email."));
-        }
+    }
 
+    public ClienteResponse atualizarCliente(Long id, ClienteRequest request) {
+        Cliente c = clienteRepository.findById(id)
+                .orElseThrow(() -> new ClienteNotFoundException(("Cliente não encontrado")));
 
+        c.setEmail(request.getEmail());
+        c.setNome(request.getNome());
+
+        clienteRepository.save(c);
+        return mapper.clienteToResponse(c);
+    }
 
 }
